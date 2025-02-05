f6217d336803af9f1b9c36712584c63561cc5bd7
xmrsv <88881293+xmrsv@users.noreply.github.com>
2025-02-05 12:17:27 -0500

    refactor

---
+++ src/database/database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE IF NOT EXISTS `mp_database`
--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ const TOKEN = process.env.MERCADOPAGO_ACCESS_TOKEN;
 
 console.log(TOKEN);
 
+const app = express();
+app.use(
+	cors({
+		origin: "*",
+	})
+);
+app.use(morgan("dev"));
+app.use(express.json());
+
 const mercadopago = new MercadoPagoConfig({
 	accessToken: TOKEN,
 });
@@ -19,8 +28,8 @@ async function enviar() {
 				items: [
 					{
 						id: "queso",
-						title: "popopipi",
-						unit_price: 9999,
+						title: "iPhone 16 Pro Max",
+						unit_price: 7099,
 						quantity: 1,
 					},
 				],
@@ -37,23 +46,47 @@ async function enviar() {
 	}
 }
 
-const app = express();
-app.use(
-	cors({
-		origin: "*",
-	})
-);
-app.use(morgan("dev"));
-app.use(express.json());
+async function findPaymentById(paymentId) {
+	try {
+		const payment = await new Payment(mercadopago).get({ id: paymentId });
+		console.log(payment);
+		return payment.status;
+	} catch (error) {
+		console.info(error);
+		return null;
+	}
+}
+
+async function isValidPayment(paymentId) {
+	const paymentStatus = await findPaymentById(paymentId);
+	if (paymentStatus === "approved") {
+		return true;
+	}
+	return false;
+}
+
+async function processPayment(paymentId) {
+	const isValid = await isValidPayment(paymentId);
+	if (isValid) {
+		console.log("Payment is valid");
+	} else {
+		console.log("Payment is not valid");
+	}
+}
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
-app.post("/checkout", async (req, res) => {
+app.post("/api/payment/init", async (req, res) => {
 	const init_point = await enviar();
-
 	res.send(init_point).status(200);
+});
+
+app.post("/api/payment/webhook", async (req, res) => {
+	const { data } = req.body;
+	console.log(data);
+	res.send({}).status(200);
 });
 
 app.listen(PORT, () => {
